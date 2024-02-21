83f3c86fad23cb415f9d13b340407c8d317cc563
AndrejTesten <andrej.testen33@gmail.com>
2024-02-21 03:50:29 +0100

    Almost finished project
    Need to fix bugs:
    Cannot add more than one or two books
    Fix styling
    Insert functions for the filters on the right side

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// array for all books
+const myLibrary = [];
+
+//book object
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -32,5 +36,55 @@ Book.prototype.info = function() {
     return (this.getTitle() + " by " + this.getAuthor() + ", " + this.getPages() + ", " + this.getRead());
 }
 
-const book1 = new Book('asd','asd',123,true);
-console.log(book1.info());
+
+const buttonAdd = document.getElementById('add-card');
+
+//
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+buttonAdd.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.getElementById("addBookButton").addEventListener("click", function() {
+    var title = document.getElementById("titleInput").value;
+    var author = document.getElementById("authorInput").value;
+    var pages = document.getElementById("pagesInput").value;
+
+    var book = new Book(title,author,pages, false);
+    myLibrary.push(book);
+
+    addBookToPage(book);
+
+    modal.style.display="none";
+
+});
+
+function addBookToPage(book) {
+    const mainElement = document.querySelector('.main');
+    event.preventDefault();
+    var newBook = document.createElement('div');
+    newBook.classList.add('cards');
+    var titleElement = document.createElement('h2');
+    titleElement.textContent = `Title: ${book.title}`;
+    var authorElement = document.createElement('h2');
+    authorElement.textContent = `Author: ${book.author}`;
+    var pagesElement = document.createElement('h2');
+    pagesElement.textContent = `Pages: ${book.pages}`;
+
+    newBook.appendChild(titleElement);
+    newBook.appendChild(authorElement);
+    newBook.appendChild(pagesElement);
+    mainElement.insertBefore(newBook,buttonAdd);
+}
